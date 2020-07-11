5b6134f5632b4db027db7b0a9fcaea197087bbe6
Breno Souza <brenosouza.rosario@gmail.com>
2020-07-10 23:44:33 -0300

    configurando o ip da aplicação back

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ import { gradeRouter } from './routes/gradeRouter.js';
 import { logger } from './config/logger.js';
 import { db } from './models/index.js';
 
-(async () => {
-  try {
-    await db.mongoose.connect(db.url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    logger.info('Conectado ao banco de dados');
-  } catch (error) {
-    logger.error(`Erro ao conectar no banco de dados! ${error}`);
+(async() => {
+    try {
+        await db.mongoose.connect(db.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        logger.info('Conectado ao banco de dados');
+    } catch (error) {
+        logger.error(`Erro ao conectar no banco de dados! ${error}`);
 
-    process.exit();
-  }
+        process.exit();
+    }
 })();
 
 const app = express();
@@ -26,17 +26,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: 'http://localhost:8080',
-  })
+    cors({
+        origin: 'https://pokemon-front.herokuapp.com',
+    })
 );
 
 app.use(gradeRouter);
 
 app.get('/', (req, res) => {
-  res.send('API em execucao');
+    res.send('API em execucao');
 });
 
 app.listen(process.env.PORT || 8081, () => {
-  logger.info(`Servidor em execucao na porta ${process.env.PORT}`);
+    logger.info(`Servidor em execucao na porta ${process.env.PORT}`);
 });
